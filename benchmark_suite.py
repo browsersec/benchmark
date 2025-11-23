1e5b55051b052e2fa568f1684d2b47edfebd7ef0
Sai Sanjay <saisanjay7660@gmail.com>
2025-11-23 16:00:50 +0530

    feat: add tqdm progress with gpu less selenium headless
    Signed-off-by: Sai Sanjay <saisanjay7660@gmail.com>

--- benchmark_suite.py
+++ benchmark_suite.py
@@ -117,7 +117,8 @@ class KubeBrowseTest:
         self.duration = duration
         options = webdriver.ChromeOptions()
         if headless:
-            options.add_argument('--headless')
+            options.add_argument('--headless=new')
+            options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=options)
